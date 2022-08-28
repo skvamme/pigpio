833b8ab0599db3d99f88794f004835a4f2b247d2
skvamme <skvammedev@gmail.com>
2022-08-28 16:44:59 +0200

    refactoring and adding server

--- src/callback.erl
+++ src/callback.erl
@@ -1,6 +1,7 @@
 -module(callback).
 -author(skvamme).
--export([make/2,init/2,loop/4]).
+%-export([make/2,init/2,loop/4]).
+-compile(export_all).
 -define(PIGPIO_IP,"127.0.0.1").
 -define(PIGPIO_PORT,8888).
 
--- src/pigpio.erl
+++ src/pigpio.erl
@@ -1,6 +1,7 @@
 -module(pigpio).
 -author(skvamme).
--export([start/0,init/0,loop/1]).
+%-export([start/0,init/0,loop/1]).
+-compile(export_all).
 -define(PIGPIO_IP,"127.0.0.1").
 -define(PIGPIO_PORT,8888).
 -define(UINT,32/little).
@@ -11,30 +12,31 @@ init() ->
         io:format("New process: ~p~n", [?MODULE]),
         inets:start(),
         {ok,Socket} = gen_tcp:connect(?PIGPIO_IP, ?PIGPIO_PORT, [binary,{packet, 0}]),
-        ok = gen_tcp:send(Socket,c(setmode,25,0)),
-        ok = gen_tcp:send(Socket,c(getmode,25)),
+        gen_tcp:send(Socket,hver()),
         %callback:make(self(),33554432), % Start a callback to monitor pin 25 on a dedicated socket
-        timer:send_interval(1000, {timer}),
         loop(Socket).
 
 loop(Socket) ->
         receive
-                {timer} -> ok = gen_tcp:send(Socket,c(br1)),
+                {servo_left,Gpio} -> ok = gen_tcp:send(Socket,servo(Gpio,500)), % Caution: Try servo_center first
+                         ?MODULE:loop(Socket);                        % Then walk your way down with servo_pos to find
+                {servo_center,Gpio} -> ok = gen_tcp:send(Socket,servo(Gpio,1500)), % the lower limit of your servo.
                          ?MODULE:loop(Socket);
+                {servo_right,Gpio} -> ok = gen_tcp:send(Socket,servo(Gpio,2500)), % Same caution here, walk your way up
+                         ?MODULE:loop(Socket);                          % to find the upper limit
+                {servo_pos,Gpio,Pos} -> ok = gen_tcp:send(Socket,servo(Gpio,Pos)), 
+                         ?MODULE:loop(Socket);                        
                 {tcp,Socket,Data} ->
                         parse(Socket,Data),
-                        ?MODULE:loop(Socket);
-                {kwh,Sec} when Sec > 0 -> 
-                        W = 3600000000 div Sec,
-                        io:format("W: ~p~n",[W]),
                         ?MODULE:loop(Socket);
                 Any -> io:format("~p got unknown msg: ~p~n",[?MODULE, Any]),
                         ?MODULE:loop(Socket)
         end.
 
-%******************************************************************************
-% pigpio response
-%******************************************************************************
+%***************************************************************************************
+% pigpio response It's easy to add a response you are missing, read
+% http://abyz.me.uk/rpi/pigpio/sif.html and http://abyz.me.uk/rpi/pigpio/pigs.html
+%***************************************************************************************
 parse(_Socket,<<>>) -> ok;
 parse(Socket,<<0:?UINT,P1:?UINT,P2:?UINT,0:?UINT,Rest/binary>>) ->
          io:format("setmode pin ~p mode ~p~n",[P1,P2]),
@@ -57,18 +59,27 @@ parse(Socket,<<10:?UINT,_P1:?UINT,_P2:?UINT,P3:?UINT,Rest/binary>>) ->
 parse(Socket,<<17:?UINT,_P1:?UINT,_P2:?UINT,P3:?UINT,Rest/binary>>) ->
          io:format("hver ~p~n",[P3]),
          parse(Socket,Rest);
-parse(_Socket,Response) -> io:format("Error: ~w~n",[Response]).
+parse(_Socket,Response) -> io:format("Unhandled response: ~w~n",[Response]).
 
-%******************************************************************************
-% pigpio commands
-%******************************************************************************
-c(hver) -> <<17:?UINT,0:?UINT,0:?UINT,0:?UINT>>;
-c(br1) -> <<10:?UINT,0:?UINT,0:?UINT,0:?UINT>>.
-c(read,Gpio) -> <<3:?UINT,Gpio:?UINT,0:?UINT,0:?UINT>>;
-c(getmode,Gpio) -> <<1:?UINT,Gpio:?UINT,0:?UINT,0:?UINT>>.
-c(setmode,Gpio,Mode) -> <<0:?UINT,Gpio:?UINT,Mode:?UINT,0:?UINT>>; % Input = 0, Output = 1
-c(write,Gpio,Level) -> <<4:?UINT,Gpio:?UINT,Level:?UINT,0:?UINT>>;
-c(setpullupdown,Gpio,Pud) -> <<2:?UINT,Gpio:?UINT,Pud:?UINT,0:?UINT>>. % Off = 0, Down = 1, Up = 2
+%**********************************************************************************************
+% pigpio commands. It's easy to add a command you are missing, read
+% http://abyz.me.uk/rpi/pigpio/sif.html and http://abyz.me.uk/rpi/pigpio/pigs.html
+%**********************************************************************************************
+hver() -> <<17:?UINT,0:?UINT,0:?UINT,0:?UINT>>. % Show hardware version
+read(Gpio) -> <<3:?UINT,Gpio:?UINT,0:?UINT,0:?UINT>>.  % Reads the current level of GPIO
+write(Gpio,Level) -> <<4:?UINT,Gpio:?UINT,Level:?UINT,0:?UINT>>. % Sets GPIO to Level The level may be 0 or 1
+setpullupdown(Gpio,Pud) -> <<2:?UINT,Gpio:?UINT,Pud:?UINT,0:?UINT>>. % Off = 0, Down = 1, Up = 2
+getmode(Gpio) -> <<1:?UINT,Gpio:?UINT,0:?UINT,0:?UINT>>. % Returns the current mode of GPIO
+setmode(Gpio,Mode) -> <<0:?UINT,Gpio:?UINT,Mode:?UINT,0:?UINT>>. % Input = 0, Output = 1
+br1() -> <<10:?UINT,0:?UINT,0:?UINT,0:?UINT>>. % Read bank 1 GPIO
+bs1(Bits) -> <<14:?UINT,Bits:?UINT>>. % Set specified GPIO in bank 1
+prs(Gpio,Range) -> <<6:?UINT,Gpio:?UINT,Range:?UINT,0:?UINT>>. % sets the dutycycle Range to be used for GPIO
+pwm(Gpio,Dutycycle) -> <<5:?UINT,Gpio:?UINT,Dutycycle:?UINT,0:?UINT>>. % Starts PWM on GPIO with Dutycycle
+pfs(Gpio,Hz) -> <<7:?UINT,Gpio:?UINT,Hz:?UINT,0:?UINT>>. % Sets the PWM frequency Hz to be used for GPIO
+servo(Gpio,Pulsewidth) -> <<8:?UINT,Gpio:?UINT,Pulsewidth:?UINT,0:?UINT>>. % Starts servo pulses of Pulsewidth microseconds on GPIO
+trig(Gpio,Pulselen,Level) -> <<37:?UINT,Gpio:?UINT,Pulselen:?UINT,4:?UINT,Level:?UINT>>. % Sends a trigger pulse of Pulselen microseconds at level to GPIO
+notifyopenib() -> <<99:?UINT,0:?UINT,0:?UINT,0:?UINT>>. % Requests a free notification handle. 
+notifybegin(Bits) -> <<19:?UINT,0:?UINT,Bits:?UINT,0:?UINT>>. % Int where Gpio is 1, other bits 0
 
 
 
